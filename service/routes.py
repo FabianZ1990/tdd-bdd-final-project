51a70928213444e2863ad075bfc71f191e3633b1
FabianZ1990 <85695169+FabianZ1990@users.noreply.github.com>
2025-03-08 21:15:31 +0100

    added update method

--- service/routes.py
+++ service/routes.py
@@ -115,18 +115,29 @@ def get_products(product_id):
     else:
         return status.HTTP_404_NOT_FOUND
 
-
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
-
 ######################################################################
 # U P D A T E   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+
+    check_content_type("application/json")
+    data = request.get_json()
+
+    product = Product().find(product_id)
+
+    if product:
+        product.deserialize(data)
+        product.id = product_id
+        product.update()
+        message = product.serialize()
+        location_url = url_for("get_products", product_id=product.id, _external=True)
+        return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    else:
+        abort(status.HTTP_404_NOT_FOUND)
+
+
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
